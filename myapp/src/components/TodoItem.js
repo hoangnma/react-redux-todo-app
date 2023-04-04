210cc375aaf755600d489b4e34854cf9d48c74f5
namlehoang <namlehoang3026@gmail.com>
2023-04-04 16:25:24 +0700

    intial commit

---
+++ myapp/src/components/TodoItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleCompleteAsync, deleteTodoAsync } from '../redux/todoSlice';
+
+const TodoItem = ({ id, title, completed }) => {
+	const dispatch = useDispatch();
+
+	const handleCheckboxClick = () => {
+		dispatch(toggleCompleteAsync({ id, completed: !completed }));
+	};
+
+	const handleDeleteClickf = () => {
+		dispatch(deleteTodoAsync({ id }));
+	}
+
+
+	return (
+		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
+			<div className='d-flex justify-content-between'>
+				<span className='d-flex align-items-center'>
+					<input type='checkbox' className='mr-3' checked={completed} onClick={handleCheckboxClick}></input>
+					{title}
+				</span>
+				<button onClick={handleDeleteClickf} className='btn btn-danger'>Delete</button>
+			</div>
+		</li>
+	);
+};
+
+export default TodoItem;
